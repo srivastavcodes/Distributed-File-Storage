ef6af0d52636b680360c7cf6247623d99157ef38
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-08-27 20:25:25 +0530

    initial commit

---
+++ p2p/tcp_transport.go
@@ -0,0 +1,20 @@
+package p2p
+
+import (
+	"net"
+	"sync"
+)
+
+type TCPTransport struct {
+	listenAddr string
+	listener   net.Listener
+
+	mu    sync.RWMutex
+	peers map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		listenAddr: listenAddr,
+	}
+}
